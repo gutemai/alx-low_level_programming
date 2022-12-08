a25044889fe405d6b67c7999b9c65f5a6d06b780
“gutemai” <gutemai430@gmail.com>
2022-12-08 15:14:39 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 /**
  * main - entry parts
- *  return 0
+ *  return -returns always 
  */
 int main(void)
-{ 
-	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}
+{	puts("\"Programming is like building a multilingual puzzle")	
+	return (0);								}
