439f8f62de5fc45ac5c88ed85ee96505abeedf47
“gutemai” <gutemai430@gmail.com>
2022-12-09 09:04:22 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * return :0
+ */
+int main(void)
+{
+	printf("size of int: %i bytes\n", sizeof(int));
+	printf("size of char:%i bytes\n", sizeof(char));
+	printf("size of float:%i bytes\n", sizeof(float));
+	printf("size of double:%i bytes\n",sizeof(double));
+	return (0);
+}
