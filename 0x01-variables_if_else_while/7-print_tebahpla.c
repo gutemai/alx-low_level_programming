aafa0222c69d916284d7e42ee0a98821f4d26350
“gutemai” <gutemai430@gmail.com>
2022-12-18 11:01:36 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+
+	putchar('\n');
+
+	return (0);
+}
