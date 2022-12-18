5c2171342f3e8210822caaf69f20c7606d9e8906
“gutemai” <gutemai430@gmail.com>
2022-12-18 12:59:39 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:Always 0 (success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 0; num1 < 9; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 10; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+
+			if (num1 == 8 && num2 == 9)
+				putchar(',');
+				putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
