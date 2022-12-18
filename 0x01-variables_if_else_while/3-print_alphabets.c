71467b71bd4bd977ec68293869eac72a1daa7138
“gutemai” <gutemai430@gmail.com>
2022-12-18 08:33:12 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry part
+ *
+ * Return:Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+	Return(0);
+}
