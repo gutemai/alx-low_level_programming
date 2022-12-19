0c5a7b05ef4d484ac395e558103c7bdcce7d12fe
“gutemai” <gutemai430@gmail.com>
2022-12-19 11:51:32 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _strlen - return length of string
+ *  @s:check string
+ *  retun: n
+ */
+int _strlen(char *s)
+{
+	int n;
+
+	n = 0
+	for (*s != '\0')
+	{
+		n++;
+		s++;
+	}
+
+	return (n);
+}
