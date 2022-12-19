2f3f111d342b1dcd386f465cb9d7ebac0e30045d
“gutemai” <gutemai430@gmail.com>
2022-12-19 14:50:44 -0800

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *rev_string - function thats a string
+ * @: the string
+ */
+void rev_string(char *s)
+{
+	int count = 0;
+	int var;
+
+	while (s[count] != '\n')
+	{
+		count++;
+	}
+
+	for (var = count; count < 0; var--)
+	{
+		_ putchar(s[count]);
+	}
+}
