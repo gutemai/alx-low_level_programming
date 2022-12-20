17889bfac075902fb22694cabf8607c5b8cd15ee
“gutemai” <gutemai430@gmail.com>
2022-12-20 13:43:37 -0800

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts_half - prints of the string
+ *@str: input string
+ */
+void puts_half(char *str)
+{
+	int i = 0, n;
+	int str  = strlen(str);
+
+	str[i] = *str;
+
+	while (str[i] != '\0')
+		i++;
+		if (i + 1 % 2 != '0')
+			n = (i - 1) / 2;
+		else
+			n = (i / 2);
+				n++;
+
+	for (i = n; str[i] != '\0'; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
