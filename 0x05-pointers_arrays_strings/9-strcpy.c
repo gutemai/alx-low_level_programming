2b1a6aa9d92439dcdeb1f95ca16c48d9c1547653
“gutemai” <gutemai430@gmail.com>
2022-12-20 15:39:01 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - function that copies to string
+ *@dest:pointer to char
+ *@src:pointer to source char
+ *Return:pointer dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
