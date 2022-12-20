f8066237ba57be4096e2c3dc7d15ef6b9ddfa55c
“gutemai” <gutemai430@gmail.com>
2022-12-20 12:32:13 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,14 +1,13 @@
 #include "main.h"
-
 /**
- * puts2 - prints 1 char of the 2 stdout 
- * @str: printed string
- * Return:0
+ * puts2 - prints an other char of string
+ * @str: input char
+ * Return:n
  */
 void puts2(char *str)
 {
 	int n;
-	
+
 	for (n = 0; str[n] != '\0'; n++)
 		if (n % 2 == 0)
 		{
