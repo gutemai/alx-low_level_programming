3db09d5fb9a1f668c4fdec94ebf01e4a4b3768f1
“gutemai” <gutemai430@gmail.com>
2022-12-23 14:20:55 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main,h"
 
 /**
- *
- *
- *
- *
+ * _strcat - concatenates the 2 string
+ * @dest:string concatenate to
+ * @src: string concatenated from
+ * Return:pointer to dest
  */
 char *_strcat(char *dest, char *src)
 {
@@ -12,8 +12,8 @@ char *_strcat(char *dest, char *src)
 	int m;
 
 	n = 0;
-	while (dest[i] != '\0')
-	{	
+	while (dest[n] != '\0')
+	{
 		n++;
 	}
 	m = 0;
