f16b58dde1881137e12e42161693607dc8a28396
“gutemai” <gutemai430@gmail.com>
2022-12-24 08:20:57 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,8 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 < 10; num2++)
 		{
-			putchar(num1 + '0');
-			putchar(num2 + '0');
+			putchar((num1 % 10) + '0');
+			putchar((num2  % 10) + '0');
 
 			if (num1 == 8 && num2 == 9)
 				continue;
@@ -25,5 +25,6 @@ int main(void)
 	}
 
 	putchar('\n');
+
 	return (0);
 }
