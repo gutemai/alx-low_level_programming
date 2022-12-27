a2963325c6df44e7f4e77e4f8a3255b412946bf8
“gutemai” <gutemai430@gmail.com>
2022-12-27 08:31:06 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @n:the numbers
+ * Return: value of last digit
+ */
+int print_last_digit(int n)
+{
+	int last_digit;
+
+	last_digit = n % 10;
+
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	_putchar(last_digit + '0');
+
+	return (0);
+}
