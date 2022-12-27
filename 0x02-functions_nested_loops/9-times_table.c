72f7570be80e8e96e10be2d62b5ebd5316ff6372
“gutemai” <gutemai430@gmail.com>
2022-12-27 09:39:37 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (i = 1; i <= 9; i++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			j = n * i;
+
+			if (j <= 9)
+				_putchar(' ');
+			else
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n);
+	}
+}
