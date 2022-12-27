f5cd927586b941687269323c89bbba29ae61775a
“gutemai” <gutemai430@gmail.com>
2022-12-27 08:43:24 -0800

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,16 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	last_digit = n % 10;
+	if (n < 0)
+	{	last_digit = (-1 * (n % 10);
+		_putchar(last_digit + '0');
+	}
 
-	if (last_digit < 0)
-		last_digit *= -1;
+	else
+	{
+		last_digit = (n % 10);
+		_putchar(last_digit + '0');
+	}
 
-	_putchar(last_digit + '0');
-
-	return (0);
+	return (last_digit);
 }
