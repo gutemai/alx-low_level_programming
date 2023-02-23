8c8fe285153a41c546d2767fa3afc2dfcbca4828
gutema <gutemai430@gmail.com>
2023-02-23 11:51:21 -0800

    a function that draws a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - draws straight line using the _ char.
+ * @n: the count of _ char printed.
+ */
+void print_line(int n)
+{
+	int length;
+
+	if (n > 0)
+	{
+		length = 0;
+		while (length < n)
+			_putchar('_');
+			length++;
+	}
+	_putchar('\n);
+}
