7adb0bdb5b518efcc93377077224467d2a609965
gutema <gutemai430@gmail.com>
2023-02-27 10:57:38 -0800

    prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,17 +1,8 @@
 #include "main.h"
-/**
- * puts2 - prints an other char of string
- * @str: input char
- * Return:n
- */
-void puts2(char *str)
-{
-	int n;
 
-	for (n = 0; str[n] != '\0'; n++)
-		if (n % 2 == 0)
-		{
-			_putchar(str[n]);
-		}
-	_putchar('\n');
-}
+/**
+ *
+ *
+ *
+ */
+
