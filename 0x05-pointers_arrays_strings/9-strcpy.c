e15101d7d4dd3563d966be2a99be063560885ad4
gutema <gutemai430@gmail.com>
2023-02-27 11:26:40 -0800

    string pointed to by src

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,10 +9,9 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
+	for (i = -1; src[i] != '\0'; i++)
 	{
 		*(dest + i) = *(src + i);
 	}
-	dust[i] = '\0';
 	return (dest);
 }
