752f1a310d41881da3dd72073f3e78e84fa3d832
gutema <gutemai430@gmail.com>
2023-03-03 09:11:42 -0800

    task1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,7 +17,7 @@ char *_strcat(char *dest, char *src)
 
 	while (src[j] != '\0')
 	{
-		dest[i + j] = src[i];
+		dest[i + j] = src[j];
 		j++;
 	}
 	dest[i + j] = '\0';
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int _strcmp(char *s1, char *s2)
+{
+
