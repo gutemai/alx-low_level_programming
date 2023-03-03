d6c8b0981e1c9991fabb580a28724f0bb6f05685
gutema <gutemai430@gmail.com>
2023-03-03 09:30:25 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,12 +9,13 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0;
+       int j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-
-	j = 0;
-
+	while (dest[i] != '\0')
+	{
+		i++
+	}
 	while (src[j] != '\0')
 	{
 		dest[i + j] = src[j];
