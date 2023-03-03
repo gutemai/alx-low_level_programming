114885268edc7db826f0add71c5677fcf807bbef
gutema <gutemai430@gmail.com>
2023-03-03 10:37:24 -0800

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *reverse_array - reverses the content of an array of integers
+ *@a:an array of integer
+ *@n:the number of elements of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i
+	int temp;
+
+	n = n - 1;
+	for (i = 0;  i < n ; i++)
+	{
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
+		n--;
+
+	}
+}
