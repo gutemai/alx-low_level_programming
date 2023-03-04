8af6755c78b6d5b73753cd72fbf6cf0f6cb90fac
gutema <gutemai430@gmail.com>
2023-03-04 09:04:38 -0800

    task3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,12 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		if (s1[i] != s2[i]
+		if (s1[i] != s2[i])
+		{
 			return (s1[i] - s2[i]);
+		}
+		*s1++;
+		*s2++;
 	}
 	return (0);
 }
