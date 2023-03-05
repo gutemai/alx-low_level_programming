7a6748ce5e6bf9f6c72995bd794fb8f74a467fd3
gutema <gutemai430@gmail.com>
2023-03-05 08:12:53 -0800

    task4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,11 +9,11 @@ void reverse_array(int *a, int n)
 	int i
 	int temp;
 
-	for (i = n - 1;  i >= n / 2 ; i--)
+	for (i = 0; i < n-- ; i++)
 	{
-		temp = a[n - 1 - i];
-		a[n - 1 - i] = a[i];
-		a[i] = temp;
+		temp = a[i];
+		a[i] = a[n];
+		a[n] = temp;
 
 	}
 }
