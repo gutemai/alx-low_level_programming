0e0bd61f7c0335d11b72aa1f85a15bf0ff0e0b25
gutema <gutemai430@gmail.com>
2023-03-12 11:58:22 -0700

    str to int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,33 +1,27 @@
 #include "main.h"
 /**
+ * _atoi - convert a string into an integer.
  *
+ * @s: the string to use.
  *
- *
+ * Return: integer.
  */
 int _atoi(char *s)
 {
-	int i = 0;
-	int n = 0;
-	int signo = 1
-	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	int sign = 1, i = 0;
+	unsigned int res = 0;
+
+
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
 	{
 		if (s[i] == '-')
-			signo *= -1;
+			sign *= -1;
 		i++;
 	}
-	while ((s[i] >= '0' && s[i] <= '9') && s[i] !=0)
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
 	{
-		if (n >= 0)
-		{
-			n = n*10 - (s[i] -'0');
-			i++;
-		}
-		else
-		{
-			n = n*10 -(s[i] - '0');
-			i++;
-		}
+		res = (res * 10) + (s[i] - '0');
+		i++;
 	}
-	sogno *= -1;
-	return (n * signo);
-}
+	res *= sign;
+	return (res);
