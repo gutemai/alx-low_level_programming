9af160370b87afbfaee2fe14bfcfc4f4e9ea1584
gutema <gutemai430@gmail.com>
2023-03-14 13:12:12 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_array - func that creates an array of chars,
+ *@size:unsigned int
+ *@c:char
+ *Return: ptr to the arr, or NULL if not
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *str;
+
+	str = malloc(sizeof(char) * size);
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	str[i] = '\0';
+
+	return (str);
+}
