b06b48a4e99de1fb60a1b885b51ffc485244e86f
gutema <gutemai430@gmail.com>
2023-03-16 12:17:07 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - program that prints its name, followed by a new line
+ *@argc:numbers of args
+ *@argv:arrays of args
+ *Return:always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+
+{
+
+	printf("%s\n", *argv);
+
+	return (0);
+}
